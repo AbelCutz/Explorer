9bd70b29311384103cb5e7f85c5cb2d60a7bde34
Abel Cutz <abel125.cutz@gmail.com>
2023-04-20 21:08:27 -0400

    final update

--- scripts/index.js
+++ scripts/index.js
@@ -27,12 +27,50 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const modalCloseButton = document.querySelector("#modal-close-button");
+const profileModalCloseButton = profileEditModal.querySelector(
+  "#modal-close-button"
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile__title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile__description-input"
+);
+const profileEditFrom = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_oppend");
+function openModal() {
+  profileEditModal.classList.add("modal_opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+}
+function closeModal() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.setAttribute("src", data.link);
+  cardImageEl.setAttribute("alt", data.name);
+  cardTitleEl.textContent = data.name;
+  return cardElement;
+}
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardListEl.append(cardElement);
 });
 
-modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_oppend");
-});
+profileEditButton.addEventListener("click", openModal);
+profileModalCloseButton.addEventListener("click", closeModal);
+profileEditFrom.addEventListener("submit", handleProfileEditSubmit);
